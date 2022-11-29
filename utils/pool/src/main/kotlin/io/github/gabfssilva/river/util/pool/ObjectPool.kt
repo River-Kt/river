9365e016e78123a4e02d7fbbf4c0fc949813fc3a
gabfssilva <gabfssilva@gmail.com>
2022-11-28 22:25:27 -0300

    Add pool module

---
+++ utils/pool/src/main/kotlin/io/github/gabfssilva/river/util/pool/ObjectPool.kt
@@ -0,0 +1,36 @@
+package io.github.gabfssilva.river.util.pool
+
+import java.time.ZonedDateTime
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.toJavaDuration
+
+interface ObjectPool<T> {
+    suspend fun borrow(): ObjectHolder<T>
+    suspend fun release(holder: ObjectHolder<T>)
+    suspend fun close()
+
+    class ObjectHolder<T>(
+        val instance: T,
+        val maxDuration: Duration,
+        val createdAt: ZonedDateTime
+    ) {
+        fun shouldBeClosed(): Boolean =
+            ZonedDateTime.now() >= createdAt.plus(maxDuration.toJavaDuration())
+    }
+
+    suspend fun <R> use(f: suspend (T) -> R): R {
+        val obj = borrow()
+        return f(obj.instance).also { release(obj) }
+    }
+
+    companion object {
+        suspend fun <T> sized(
+            maxSize: Int,
+            initialSize: Int = 1,
+            maxDuration: Duration = 5.minutes,
+            onClose: suspend (T) -> Unit = {},
+            factory: suspend () -> T,
+        ) = DefaultObjectPool(maxSize, maxDuration, (1..initialSize).map { factory() }, factory, onClose)
+    }
+}
