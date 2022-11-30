5f24668f8c816468753537c021ad1cdc65df1c27
gabfssilva <gabfssilva@gmail.com>
2022-11-29 23:30:36 -0300

    Improve HTTP dsl a bit

--- utils/http/src/main/kotlin/io/github/gabfssilva/river/util/http/RequestBuilder.kt
+++ utils/http/src/main/kotlin/io/github/gabfssilva/river/util/http/RequestBuilder.kt
@@ -6,10 +6,12 @@ import java.net.URI
 import java.net.http.HttpRequest
 
 class RequestBuilder(
-    private val url: String,
-    private val method: String,
-    private var body: () -> Flow<ByteArray> = { emptyFlow() },
-    private val headers: MutableMap<String, List<String>> = mutableMapOf()
+    val url: String,
+    val method: String,
+    var contentType: String? = null,
+    var body: () -> Flow<ByteArray> = { emptyFlow() },
+    val query: MutableMap<String, List<String>> = mutableMapOf(),
+    val headers: MutableMap<String, List<String>> = mutableMapOf()
 ) {
     fun body(f: () -> Flow<ByteArray>) {
         body = f
@@ -17,15 +19,46 @@ class RequestBuilder(
 
     fun contentType(s: String) = header("Content-Type", s)
 
+    private fun uri() =
+        query
+            .toList()
+            .flatMap { (key, values) -> values.map { key to it } }
+            .joinToString(separator = "&", prefix = "?") { (key, value) -> "$key=$value" }
+            .let { URI("$url$it") }
+
     fun build(): HttpRequest =
         HttpRequest
-            .newBuilder(URI(url))
+            .newBuilder(uri())
             .method(method, body().asBodyPublisher())
             .also { builder ->
                 headers.forEach { (key, values) -> values.forEach { value -> builder.setHeader(key, value) } }
             }
             .build()
 
-    fun header(key: String, values: List<String>) = headers.put(key, values)
-    fun header(key: String, vararg values: String) = headers.put(key, values.toList())
+    fun query(
+        key: String,
+        values: List<String>
+    ) = query.put(key, values)
+
+    fun query(
+        key: String,
+        vararg values: String?
+    ) = values
+        .mapNotNull { it }
+        .let { if (it.isNotEmpty()) query(key, it) }
+
+    fun query(
+        vararg parameters: Pair<String, String?>,
+    ) = parameters
+        .map { (key, value) -> query(key, value) }
+
+    fun header(
+        key: String,
+        values: List<String>
+    ) = headers.put(key, values)
+
+    fun header(
+        key: String,
+        vararg values: String
+    ) = header(key, values.toList())
 }
