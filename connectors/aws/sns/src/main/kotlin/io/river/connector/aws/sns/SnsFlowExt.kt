0478add138b6e8d9e6e2743bd74c3dcfb6745f42
gabfssilva <gabfssilva@gmail.com>
2022-12-15 21:45:39 -0300

    Adjusting docs

--- connectors/aws/sns/src/main/kotlin/io/river/connector/aws/sns/SnsFlowExt.kt
+++ connectors/aws/sns/src/main/kotlin/io/river/connector/aws/sns/SnsFlowExt.kt
@@ -1,11 +1,14 @@
 package io.river.connector.aws.sns
 
-import io.river.core.*
+import io.river.core.ChunkStrategy
+import io.river.core.chunked
+import io.river.core.mapParallel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.future.await
 import software.amazon.awssdk.services.sns.SnsAsyncClient
 import software.amazon.awssdk.services.sns.model.MessageAttributeValue
 import software.amazon.awssdk.services.sns.model.PublishBatchRequestEntry
+import software.amazon.awssdk.services.sns.model.PublishBatchResponse
 import java.util.*
 import kotlin.time.Duration.Companion.milliseconds
 
@@ -14,21 +17,20 @@ fun SnsAsyncClient.publishFlow(
     topicArn: String,
     parallelism: Int = 1,
     chunkStrategy: ChunkStrategy = ChunkStrategy.TimeWindow(10, 250.milliseconds)
-) = chunked(chunkStrategy)
-    .mapParallel(parallelism) {
-        publishBatch { builder ->
-            builder
-                .publishBatchRequestEntries(it)
-                .topicArn(topicArn)
-        }.await()
-    }
+): Flow<PublishBatchResponse> =
+    chunked(chunkStrategy)
+        .mapParallel(parallelism) {
+            publishBatch { builder -> builder.publishBatchRequestEntries(it).topicArn(topicArn) }
+                .await()
+        }
 
 fun SnsAsyncClient.publishFlow(
     topicArn: String,
     upstream: Flow<PublishBatchRequestEntry>,
     parallelism: Int = 1,
     chunkStrategy: ChunkStrategy = ChunkStrategy.TimeWindow(10, 250.milliseconds)
-) = with(upstream) { publishFlow(topicArn, parallelism, chunkStrategy) }
+): Flow<PublishBatchResponse> =
+    with(upstream) { publishFlow(topicArn, parallelism, chunkStrategy) }
 
 fun PublishRequestEntry(
     message: String,
