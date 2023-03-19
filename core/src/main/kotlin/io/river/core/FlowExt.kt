267b956a1055ab79d0c119cca32da80c6e0383fa
gabfssilva <gabfssilva@gmail.com>
2023-03-19 17:21:47 -0300

    Add docs

--- core/src/main/kotlin/io/river/core/FlowExt.kt
+++ core/src/main/kotlin/io/river/core/FlowExt.kt
@@ -9,7 +9,6 @@ import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.*
 import kotlin.time.Duration
 
-
 fun <T> Flow<T>.timeout(duration: Duration): Flow<T> =
     flow { withTimeoutOrNull(duration) { collect { emit(it) } } }
 
@@ -104,15 +103,42 @@ suspend fun <T> Flow<T>.toList(
         halt()
     }.toList()
 
+/**
+ * The [chunked] function is used to split the elements emitted by the current [Flow] into chunks.
+ *
+ * The chunks are emitted as a [List] of elements, with each chunk containing size elements or each chunk lasting
+ * duration time, depending on the [ChunkStrategy] provided.
+ **
+ * Note that the chunks may not be of exactly the same size, depending on the number of elements emitted and the
+ * specified size. Additionally, the order of elements in the output flow is preserved from the input flow.
+ */
+fun <T> Flow<T>.chunked(strategy: ChunkStrategy): Flow<List<T>> =
+    Chunk(this, strategy)
+
+/**
+ * The [windowedChunk] function is used to split the elements emitted by the current [Flow] into fixed size chunks based
+ * on a time window.
+ *
+ * The chunks are emitted as a [List] of elements, with each chunk containing size elements or each chunk lasting
+ * duration time.
+ *
+ * Note that the chunks may not be of exactly the same size, depending on the number of elements emitted and the
+ * specified size. Additionally, the order of elements in the output flow is preserved from the input flow.
+ */
 fun <T> Flow<T>.windowedChunk(
     size: Int,
     duration: Duration
 ): Flow<List<T>> =
     chunked(ChunkStrategy.TimeWindow(size, duration))
 
-fun <T> Flow<T>.chunked(strategy: ChunkStrategy): Flow<List<T>> =
-    Chunk(this, strategy)
-
+/**
+ * The [chunked] function is used to split the elements emitted by the current [Flow] into chunks of a fixed size.
+ *
+ * The chunks are emitted as a [List] of elements, with each chunk containing size elements.
+ *
+ * Note that the chunks may not be of exactly the same size, depending on the number of elements emitted and the
+ * specified size. Additionally, the order of elements in the output flow is preserved from the input flow.
+ */
 fun <T> Flow<T>.chunked(size: Int): Flow<List<T>> =
     chunked(ChunkStrategy.Count(size))
 
@@ -144,31 +170,95 @@ suspend fun <T> Flow<T>.countOnWindow(duration: Duration): Int {
     return withTimeoutOrNull(duration) { collect { counter++ }; counter } ?: counter
 }
 
+/**
+ * The [mapParallel] function is similar to the [map] function
+ * since it transforms each element via the [transform] function.
+ *
+ * It works, however, in a parallel way, which means that multiple elements can be processed at the same time,
+ * especially useful for more intensive tasks.
+ *
+ * Use [concurrencyLevel] to configure the parallelism number.
+ *
+ * One thing to note is that the order of the elements is preserved,
+ * so the output flow will contain the same elements as the input flow,
+ * but with the values transformed according to the provided function.
+ */
 fun <T, R> Flow<T>.mapParallel(
     concurrencyLevel: Int,
-    f: suspend ConcurrencyInfo.(T) -> R
-): Flow<R> = MapParallelFlow(this, concurrencyLevel, f)
+    transform: suspend ConcurrencyInfo.(T) -> R
+): Flow<R> = MapParallelFlow(this, concurrencyLevel, transform)
 
+/**
+ * The [unorderedMapParallel] function is similar to the [mapParallel] function in that it transforms each element
+ * in a parallel way using the provided [f] function. However, unlike [mapParallel], this function does not guarantee
+ * that the output elements will be in the same order as the input elements. This means that this function can be
+ * significantly faster than [mapParallel] because it does not have to preserve order.
+ *
+ * Use [concurrencyLevel] to configure the parallelism number.
+ */
 fun <T, R> Flow<T>.unorderedMapParallel(
     concurrencyLevel: Int,
     f: suspend ConcurrencyInfo.(T) -> R
 ): Flow<R> = UnorderedMapParallelFlow(this, concurrencyLevel, f)
 
-fun <T, R> Flow<Iterable<T>>.unorderedFlatMapParallel(
-    concurrencyLevel: Int,
-    f: suspend ConcurrencyInfo.(Iterable<T>) -> Iterable<R>
-): Flow<R> = mapParallel(concurrencyLevel, f).flatten()
-
+/**
+ * The [flatMapParallel] function is similar to the [flatMap] function but works in a parallel way
+ * to transform each element of the [Flow], which is an Iterable, with the provided [f] function.
+ *
+ * This function transforms each Iterable element of the input Flow by applying the [f] function in a
+ * parallel way. This means that multiple elements can be processed at the same time, especially useful
+ * for more intensive tasks.
+ *
+ * Use [concurrencyLevel] to configure the parallelism number.
+ *
+ * The output of this function is a Flow of the transformed elements, where the order of the elements is preserved.
+ * This means that the output Flow will contain the same elements as the input Flow, but with each element
+ * transformed according to the provided function.
+ */
 fun <T, R> Flow<Iterable<T>>.flatMapParallel(
     concurrencyLevel: Int,
     f: suspend ConcurrencyInfo.(Iterable<T>) -> Iterable<R>
 ): Flow<R> = mapParallel(concurrencyLevel, f).flatten()
 
+/**
+ * The [unorderedFlatMapParallel] function is similar to the [flatMapParallel] function but does not guarantee
+ * the order of the output elements.
+ *
+ * This function transforms each Iterable element of the input Flow by applying the [f] function in a parallel way.
+ * This means that multiple elements can be processed at the same time, especially useful for more intensive tasks.
+ *
+ * Use [concurrencyLevel] to configure the parallelism number.
+ *
+ * The output of this function is a Flow of the transformed elements, where the order of the elements is not preserved.
+ * This means that the output Flow may not contain the elements in the same order as the input Flow.
+ * However, this function can be significantly faster than [flatMapParallel] because it does not have to preserve order.
+ */
+fun <T, R> Flow<Iterable<T>>.unorderedFlatMapParallel(
+    concurrencyLevel: Int,
+    f: suspend ConcurrencyInfo.(Iterable<T>) -> Iterable<R>
+): Flow<R> = mapParallel(concurrencyLevel, f).flatten()
+
+/**
+ * The [collectAsync] function launches a coroutine to collect the elements emitted by the current [Flow] in an asynchronous way.
+ *
+ * This function returns a [Job] instance that represents the coroutine launched to collect the elements.
+ *
+ * The coroutine will continue running until the flow completes or an exception is thrown.
+ *
+ * If the caller needs to cancel the coroutine before it completes, they can cancel the returned [Job].
+ */
 fun <T> Flow<T>.collectAsync(
     scope: CoroutineScope = CoroutineScope(Dispatchers.Default),
     collector: FlowCollector<T> = FlowCollector { }
 ): Job = scope.launch { collect(collector) }
 
+/**
+ * The [collectCatching] function collects the elements emitted by the current [Flow] in a suspending way and returns
+ * a [Result] instance that represents the result of the operation.
+ *
+ * If the collection of elements completes successfully, the function returns a [Result] instance with a value of [Unit].
+ * Otherwise, it returns a [Result] instance with the corresponding error.
+ */
 suspend fun <T> Flow<T>.collectCatching(
     collector: FlowCollector<T> = FlowCollector { },
 ): Result<Unit> = runCatching { collect(collector) }
