dd4b08db006bcbcce1dfb438ebbcd48fcbd71fcf
gabfssilva <gabfssilva@gmail.com>
2023-04-01 14:44:27 -0300

    Add docs for JDBC connector

--- connectors/rdbms/jdbc/src/main/kotlin/io/river/connector/rdbms/jdbc/JdbcExt.kt
+++ connectors/rdbms/jdbc/src/main/kotlin/io/river/connector/rdbms/jdbc/JdbcExt.kt
@@ -16,6 +16,27 @@ import kotlin.time.Duration.Companion.milliseconds
 
 typealias Row = Map<String, Any?>
 
+/**
+ * Executes a single SQL update using a JDBC connection.
+ *
+ * This function takes an SQL statement [sql] and an optional [prepare] function, which prepares
+ * the statement before execution.
+ *
+ * @param sql The SQL statement to execute.
+ * @param prepare A suspend function that prepares the statement before execution.
+ * @return A [Flow] of the number of rows affected by the update.
+ *
+ * Example:
+ * ```
+ * val jdbc: Jdbc = ...
+ * val sql = "UPDATE users SET active = 1 WHERE id = 42"
+ *
+ * jdbc.singleUpdate(sql)
+ *     .collect { rowsAffected ->
+ *         println("Rows affected: $rowsAffected")
+ *     }
+ * ```
+ */
 fun Jdbc.singleUpdate(
     sql: String,
     prepare: suspend PreparedStatement.() -> Unit = {}
@@ -25,6 +46,31 @@ fun Jdbc.singleUpdate(
         .let { emit(it) }
 }
 
+/**
+ * Executes a single SQL update using a JDBC connection for each item in the [upstream] flow.
+ *
+ * This function takes an SQL statement [sql], an [upstream] flow, [parallelism], and an optional
+ * [prepare] function, which prepares the statement before execution.
+ *
+ * @param sql The SQL statement to execute.
+ * @param upstream A [Flow] of items to process.
+ * @param parallelism The level of parallelism for executing the updates.
+ * @param prepare A suspend function that prepares the statement for each item before execution.
+ * @return A [Flow] of the number of rows affected by the update for each item.
+ *
+ * Example:
+ * ```
+ * val jdbc: Jdbc = ...
+ * val sql = "UPDATE users SET active = 1 WHERE id = ?"
+ * val userIds = flowOf(1, 2, 3, 4, 5)
+ *
+ * jdbc.singleUpdate(sql, userIds, prepare = { id ->
+ *     setInt(1, id)
+ * }).collect { rowsAffected ->
+ *     println("Rows affected: $rowsAffected")
+ * }
+ * ```
+ */
 fun <T> Jdbc.singleUpdate(
     sql: String,
     upstream: Flow<T>,
@@ -42,6 +88,29 @@ fun <T> Jdbc.singleUpdate(
         }
     }
 
+/**
+ * Executes a batch update for the given SQL statement using the provided [upstream] Flow as input.
+ * The batch update is performed in chunks, as specified by the [chunkStrategy] parameter, and can be executed in parallel
+ * using the specified [parallelism] level.
+ *
+ * @param sql the SQL statement to be executed
+ * @param upstream the Flow of input elements to be used in the batch update
+ * @param parallelism the level of parallelism to be used in the batch update (default: 1)
+ * @param chunkStrategy the chunking strategy to be used for processing the input elements (default: TimeWindow(100, 250.milliseconds))
+ * @param prepare a function that prepares the PreparedStatement using the current input element (default: {})
+ * @return A [Flow] of the number of rows affected by the update for each batch.
+ *
+ * Example usage:
+ *
+ * ```
+ *  val data = flowOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+ *  val updatedRows = Jdbc.batchUpdate("UPDATE my_table SET value = ? WHERE id = ?", data) { ps, value ->
+ *      ps.setInt(1, value)
+ *      ps.setInt(2, value)
+ *  }
+ *  updatedRows.collect { println("Updated $it rows.") }
+ * ```
+ */
 fun <T> Jdbc.batchUpdate(
     sql: String,
     upstream: Flow<T>,
@@ -65,11 +134,50 @@ fun <T> Jdbc.batchUpdate(
             }
         }
 
+/**
+ * Executes a typed query for the given SQL statement using the provided [fetchSize] as a result set fetch size.
+ * The resulting [Flow] emits objects of type [T], which must have a primary constructor and whose parameters are
+ * matched with the columns of the result set.
+ *
+ * @param sql the SQL statement to be executed
+ * @param fetchSize the result set fetch size to be used in the query (default: 100)
+ * @return a Flow of objects of type [T], whose parameters are matched with the columns of the result set.
+ *
+ * Example usage:
+ *
+ * ```
+ *  data class Person(val name: String, val age: Int)
+ *
+ *  val people = Jdbc.typedQuery<Person>("SELECT name, age FROM people WHERE age > 18")
+ *  people.collect { println(it) }
+ * ```
+ */
 inline fun <reified T : Any> Jdbc.typedQuery(
     sql: String,
     fetchSize: Int = 100,
 ): Flow<T> = typedQuery(sql, fetchSize) { }
 
+/**
+ * Executes a typed query for the given SQL statement using the provided [fetchSize] as a result set fetch size.
+ * The resulting [Flow] emits objects of type [T], which must have a primary constructor and whose parameters are
+ * matched with the columns of the result set.
+ *
+ * @param sql the SQL statement to be executed
+ * @param fetchSize the result set fetch size to be used in the query (default: 100)
+ * @param prepare a function that prepares the PreparedStatement for the query (default: {})
+ * @return a Flow of objects of type [T], whose parameters are matched with the columns of the result set.
+ *
+ * Example usage:
+ * ```
+ *  data class Person(val name: String, val age: Int)
+ *
+ *  val people = Jdbc.typedQuery<Person>("SELECT name, age FROM people WHERE age > ?") {
+ *      setInt(1, 18)
+ *  }
+ *
+ *  people.collect { println(it) }
+ * ```
+ */
 inline fun <reified T : Any> Jdbc.typedQuery(
     sql: String,
     fetchSize: Int = 100,
@@ -86,6 +194,22 @@ inline fun <reified T : Any> Jdbc.typedQuery(
             )
         }
 
+/**
+ * Executes a query for the given SQL statement using the provided [fetchSize] as a result set fetch size.
+ *
+ * @param sql the SQL statement to be executed
+ * @param fetchSize the result set fetch size to be used in the query (default: 100)
+ * @param prepare a function that prepares the PreparedStatement for the query (default: {})
+ * @return a Flow of Row objects, representing the result set.
+ *
+ * Example usage:
+ * ```
+ *  val result = Jdbc.query("SELECT * FROM my_table WHERE id = ?") {
+ *      setInt(1, 10)
+ *  }
+ *  result.collect { println(it) }
+ *```
+ */
 fun Jdbc.query(
     sql: String,
     fetchSize: Int = 100,
