27680bf55edc4d271986269d33c1a23046d88147
gabfssilva <gabfssilva@gmail.com>
2023-04-01 14:36:18 -0300

    Add docs for AWS sqs

--- connectors/aws/sqs/src/main/kotlin/io/river/connector/aws/sqs/SqsFlowExt.kt
+++ connectors/aws/sqs/src/main/kotlin/io/river/connector/aws/sqs/SqsFlowExt.kt
@@ -18,28 +18,28 @@ internal val SqsAsyncClient.logger: Logger
     get() = LoggerFactory.getLogger(javaClass)
 
 /**
- * Returns a Flow of messages from an Amazon SQS queue using the AWS SDK asynchronous client.
+ * Continuously receives messages from an Amazon Simple Queue Service (SQS) queue using the provided [SqsAsyncClient].
+ * The received messages are returned as a [Flow] of [Message] objects.
  *
- * Customize the polling parallelism providing custom [minimumParallelism], [maxParallelism] and [increaseStrategy] values.
+ * @param maxParallelism The maximum number of parallel receive operations. Defaults to 1.
+ * @param stopOnEmptyList If true, the flow will stop when an empty list of messages is received. Defaults to false.
+ * @param minimumParallelism The minimum number of parallel receive operations. Defaults to 1.
+ * @param increaseStrategy Determines how the parallelism increases when processing messages. Defaults to [ParallelismIncreaseStrategy.ByOne].
+ * @param builder A lambda with receiver for configuring the [ReceiveMessageRequestBuilder] for the underlying receive operation.
  *
- * If you want the Flow to stop consuming the queue if the queue is empty, provide [stopOnEmptyList] as true.
+ * @return A [Flow] of [Message] objects representing the messages received from the SQS queue.
  *
  * Example usage:
  *
  * ```
- * val sqsClient = SqsAsyncClient.create()
+ *  val sqsClient = SqsAsyncClient.create()
+ *  val queueUrl = "https://sqs.us-east-1.amazonaws.com/123456789012/myqueue"
  *
- * val queueUrl = sqsClient.getQueueUrl { it.queueUrl("my-queue") }.await().queueUrl()
- *
- * val messagesFlow = sqsClient.receiveMessagesFlow {
- *     queueUrl(queueUrl)
- *     maxNumberOfMessages(10)
- *     waitTimeSeconds(20)
- * }
- *
- * messagesFlow.collect { message ->
- *     println("Received message: ${message.body()}")
- * }
+ *  sqsClient.receiveMessagesFlow {
+ *      queueUrl(queueUrl)
+ *      maxNumberOfMessages(10)
+ *      waitTimeSeconds(20)
+ *  }.collect { message -> println("Received message: ${message.body()}") }
  * ```
  */
 fun SqsAsyncClient.receiveMessagesFlow(
@@ -65,6 +65,69 @@ fun SqsAsyncClient.receiveMessagesFlow(
             }
         }
 
+/**
+ * Sends messages to an Amazon Simple Queue Service (SQS) queue using the provided [SqsAsyncClient].
+ * Messages are consumed from an upstream [Flow] of [RequestMessage] objects.
+ *
+ * @param queueUrl The URL of the Amazon SQS queue to which messages will be sent.
+ * @param upstream A [Flow] of [RequestMessage] objects to be sent to the specified SQS queue.
+ * @param parallelism The number of concurrent send operations. Defaults to 1.
+ * @param chunkStrategy Determines how to group messages for sending in batches. Defaults to [ChunkStrategy.TimeWindow].
+ *
+ * @return A [Flow] of [SendMessageBatchResponse] objects representing the results of sending messages in batches.
+ *
+ * Example usage:
+ *
+ * ```
+ *  val sqsClient = SqsAsyncClient.create()
+ *  val queueUrl = "https://sqs.us-east-1.amazonaws.com/123456789012/myqueue"
+ *  val messages = flowOf(RequestMessage("Message 1"), RequestMessage("Message 2"), RequestMessage("Message 3"))
+ *
+ *  sqsClient
+ *      .sendMessageFlow(queueUrl, messages)
+ *      .collect { response -> println("Batch sent with messageId: ${response.batchItemId()}") }
+ * ```
+ */
+fun SqsAsyncClient.sendMessageFlow(
+    queueUrl: String,
+    upstream: Flow<RequestMessage>,
+    parallelism: Int = 1,
+    chunkStrategy: ChunkStrategy = ChunkStrategy.TimeWindow(10, 250.milliseconds)
+) = upstream
+    .map { it.asMessageRequestEntry() }
+    .chunked(chunkStrategy)
+    .mapParallel(parallelism) { entries ->
+        sendMessageBatch { it.queueUrl(queueUrl).entries(entries) }
+            .await()
+    }
+
+/**
+ * Creates a flow that changes the visibility of messages in an Amazon SQS queue.
+ *
+ * This function takes an [upstream] flow of [MessageAcknowledgment] objects and processes them
+ * in parallel using [parallelism] and the specified [chunkStrategy].
+ *
+ * @param queueUrl The URL of the SQS queue.
+ * @param upstream A [Flow] of [MessageAcknowledgment] objects.
+ * @param parallelism The level of parallelism for processing messages.
+ * @param chunkStrategy The strategy to use when chunking messages for processing.
+ * @return A [Flow] that emits pairs of [MessageAcknowledgment] and [ChangeMessageVisibilityBatchResponse].
+ *
+ * Example usage:
+ *
+ * ```
+ * val sqsClient = SqsAsyncClient.create()
+ * val queueUrl = "https://sqs.region.amazonaws.com/123456789012/queue-name"
+ *
+ * val acknowledgmentsFlow: Flow<MessageAcknowledgment<ChangeMessageVisibility>> = // ... create a flow
+ *
+ * val resultFlow = sqsClient.changeMessageVisibilityFlow(queueUrl, acknowledgmentsFlow)
+ *
+ * resultFlow.collect { (acknowledgment, response) ->
+ *     // Process the acknowledgment and response
+ * }
+ * ```
+ */
 fun SqsAsyncClient.changeMessageVisibilityFlow(
     queueUrl: String,
     upstream: Flow<MessageAcknowledgment<ChangeMessageVisibility>>,
@@ -91,19 +154,33 @@ fun SqsAsyncClient.changeMessageVisibilityFlow(
     }
     .flatten()
 
-fun SqsAsyncClient.sendMessageFlow(
-    queueUrl: String,
-    upstream: Flow<RequestMessage>,
-    parallelism: Int = 1,
-    chunkStrategy: ChunkStrategy = ChunkStrategy.TimeWindow(10, 250.milliseconds)
-) = upstream
-    .map { it.asMessageRequestEntry() }
-    .chunked(chunkStrategy)
-    .mapParallel(parallelism) { entries ->
-        sendMessageBatch { it.queueUrl(queueUrl).entries(entries) }
-            .await()
-    }
-
+/**
+ * Creates a flow that deletes messages from an Amazon SQS queue.
+ *
+ * This function takes an [upstream] flow of [MessageAcknowledgment] objects with a [Delete]
+ * acknowledgment and processes them in parallel using [parallelism] and the specified [chunkStrategy].
+ *
+ * @param queueUrl The URL of the SQS queue.
+ * @param upstream A [Flow] of [MessageAcknowledgment] objects with a [Delete] acknowledgment.
+ * @param parallelism The level of parallelism for processing messages.
+ * @param chunkStrategy The strategy to use when chunking messages for processing.
+ * @return A [Flow] that emits pairs of [MessageAcknowledgment] and [DeleteMessageBatchResponse].
+ *
+ * Example usage:
+ * ```
+ * val sqsClient: SqsAsyncClient = ...
+ * val queueUrl = "https://sqs.us-east-1.amazonaws.com/123456789012/myqueue"
+ * val messageAcknowledgments = flowOf(
+ *     MessageAcknowledgment(Message(...), Delete),
+ *     MessageAcknowledgment(Message(...), Delete)
+ * )
+ *
+ * sqsClient.deleteMessagesFlow(queueUrl, messageAcknowledgments)
+ *     .collect { (ack, response) ->
+ *         println("Deleted message: ${ack.message.messageId()}, response: $response")
+ *     }
+ * ```
+ */
 fun SqsAsyncClient.deleteMessagesFlow(
     queueUrl: String,
     upstream: Flow<MessageAcknowledgment<Delete>>,
@@ -134,6 +211,35 @@ fun SqsAsyncClient.deleteMessagesFlow(
         }
         .flatten()
 
+/**
+ * Creates a flow that processes acknowledgments for messages in an Amazon SQS queue.
+ *
+ * This function takes an [upstream] flow of [MessageAcknowledgment] objects and processes them
+ * based on their acknowledgment type: [ChangeMessageVisibility], [Delete], or [Ignore].
+ * The function processes acknowledgments in parallel using [parallelism] and the specified [chunkStrategy].
+ *
+ * @param queueUrl The URL of the SQS queue.
+ * @param upstream A [Flow] of [MessageAcknowledgment] objects.
+ * @param parallelism The level of parallelism for processing messages.
+ * @param chunkStrategy The strategy to use when chunking messages for processing.
+ * @return A [Flow] of [AcknowledgmentResult] objects that contain the message, acknowledgment, and response.
+ *
+ * Example usage:
+ * ```
+ * val sqsClient: SqsAsyncClient = ...
+ * val queueUrl = "https://sqs.us-east-1.amazonaws.com/123456789012/myqueue"
+ * val messageAcknowledgments = flowOf(
+ *     MessageAcknowledgment(Message(...), Delete),
+ *     MessageAcknowledgment(Message(...), ChangeMessageVisibility(30)),
+ *     MessageAcknowledgment(Message(...), Ignore)
+ * )
+ *
+ * sqsClient.acknowledgmentMessageFlow(queueUrl, messageAcknowledgments)
+ *     .collect { result ->
+ *         println("Processed message: ${result.message.messageId()}, acknowledgment: ${result.acknowledgment}, response: ${result.response}")
+ *     }
+ * ```
+ */
 fun SqsAsyncClient.acknowledgmentMessageFlow(
     queueUrl: String,
     upstream: Flow<MessageAcknowledgment<out Acknowledgment>>,
