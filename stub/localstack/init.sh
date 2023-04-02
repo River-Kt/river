5fbdf435a3a5457dd398104db5bd6d9ea0a371c4
gabfssilva <gabfssilva@gmail.com>
2023-04-02 16:47:44 -0300

    Fix zip-file lambda folder

--- stub/localstack/init.sh
+++ stub/localstack/init.sh
@@ -3,6 +3,6 @@
 awslocal lambda create-function \
   --function-name hello_world \
   --runtime python \
-  --zip-file fileb:///docker-entrypoint-initaws.d/hello_lambda.py.zip \
+  --zip-file fileb:///etc/localstack/init/ready.d/hello_lambda.py.zip \
   --handler hello_lambda.hello_handler \
   --role sample
