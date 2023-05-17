88d7ae10a7e9c40e5bdab8ae76257de43118a92b
gabfssilva <gabfssilva@gmail.com>
2023-05-17 01:10:18 -0300

    Make lines more generic by creating splitEvery

--- core/src/main/kotlin/com/river/core/FlowStringExt.kt
+++ core/src/main/kotlin/com/river/core/FlowStringExt.kt
@@ -1,8 +1,57 @@
 package com.river.core
 
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onCompletion
 import java.nio.ByteBuffer
 import java.nio.charset.Charset
+
+/**
+ * Splits and buffers the [Flow] of [String] based on the provided delimiter, emitting each piece as a separate element in the resulting [Flow].
+ *
+ * This function buffers incoming strings until it encounters the provided delimiter. It then emits each piece as a separate string.
+ * If the incoming strings do not contain any delimiter, they are buffered until the delimiter is encountered or the flow is completed.
+ *
+ * @return A new [Flow] where each element represents a piece split by a delimiter from the original [Flow] of [String].
+ *
+ * Example usage:
+ *
+ * ```
+ * val flow = flowOf(
+ *    "Hel",
+ *    "lo,",
+ *    " world!",
+ *    "\nW",
+ *    "elcome to River!"
+ * )
+ *
+ * flow
+ *     .splitEvery("\n")
+ *     .collect(::println) //"Hello, world!", "Welcome to River!"
+ * ```
+ */
+fun Flow<String>.splitEvery(
+    delimiter: String
+) = flow {
+    var buffer = ""
+
+    onCompletion { emit(delimiter) }
+        .collect { chunk ->
+            buffer += chunk
+
+            val stopsWithLineBreak = buffer.endsWith(delimiter)
+            val pieces = buffer.split(delimiter)
+
+            if (stopsWithLineBreak) {
+                pieces.filterNot { it.isBlank() }.forEach { emit(it) }
+                buffer = ""
+            } else if (pieces.size > 1) {
+                pieces.dropLast(1).forEach { emit(it) }
+                buffer = pieces.last()
+            }
+        }
+}
 
 /**
  * Splits and buffers the [Flow] of [String] into lines, emitting each line as a separate element in the resulting [Flow].
@@ -28,26 +77,7 @@ import java.nio.charset.Charset
  *     .collect(::println) //"Hello, world!", "Welcome to River!"
  * ```
  */
-fun Flow<String>.lines() = flow {
-    var buffer = ""
-
-    onCompletion { emit("\n") }
-        .collect {
-            buffer += it
-
-            val stopsWithLineBreak = buffer.endsWith("\n")
-            val lines = buffer.split("\n")
-
-            if (stopsWithLineBreak) {
-                lines.filterNot { it.isBlank() }.forEach { emit(it) }
-                buffer = ""
-            } else if (lines.size > 1) {
-                lines.dropLast(1).forEach { emit(it) }
-                buffer = lines.last()
-            }
-        }
-}
-
+fun Flow<String>.lines() = splitEvery("\n")
 
 /**
  * Converts the [Flow] of [String] to a [Flow] of [Byte] using the specified [charset].
