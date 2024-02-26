abb485e6e41136b205ea903c5970dc12f92c434e
gabfssilva <gabfssilva@gmail.com>
2024-02-26 11:05:25 -0300

    Add Ext to gradle build

---
+++ buildSrc/src/main/kotlin/Ext.kt
@@ -0,0 +1,16 @@
+import com.android.build.gradle.AppExtension
+import com.android.build.gradle.LibraryExtension
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+
+inline fun Project.onAndroid(crossinline configure: LibraryExtension.() -> Unit) {
+    pluginManager.withPlugin("com.android.library") {
+        extensions.configure<LibraryExtension> { configure() }
+    }
+}
+
+inline fun Project.androidApp(crossinline configure: AppExtension.() -> Unit) {
+    pluginManager.withPlugin("com.android.library") {
+        extensions.configure<AppExtension> { configure() }
+    }
+}
